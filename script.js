9253d4abb0b8d2737014e41986cd3c3cbfae28d2
Kolipion <kochetov@offenbach.ihk.de>
2026-07-28 11:51:57 +0200

    Add line break functionality to textarea input

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
+// Textfeld auswählen
+const textarea = document.getElementById("editor");
+
+// Nach jeweils 40 Zeichen einen Zeilenumbruch einfügen
+textarea.addEventListener("input", function(){
+
+    let text = this.value.replace(/\n/g, "");
+
+    let result = "";
+
+    for(let i = 0; i < text.length; i++){
+
+        result += text[i];
+
+        if((i + 1) % 40 === 0){
+            result += "\n";
+        }
+
+    }
+
+    this.value = result;
+
+});
+
+
+// EmailJS initialisieren
 emailjs.init({
     publicKey: "t_HBf_gztN5fNCVX0"
 });
 
+
+// Save-Button
 document.getElementById("save").addEventListener("click", function(){
 
     const text = document.getElementById("editor").value;
@@ -13,6 +41,9 @@ document.getElementById("save").addEventListener("click", function(){
             message: text
         }
     ).then(() => {
+
         alert("Gespeichert");
+
     });
+
 });
